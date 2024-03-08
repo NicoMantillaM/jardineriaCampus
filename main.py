cc3ed4533917a4a1f42ba40bddc8be8f2d1b1910
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-07 21:28:24 -0500

    feat: :construction: filtro para los pagos en paypal
    hice un filtro que me imprime unicamente un listado con los pagos que se realizaron en el año 2008 y por medio de paypal, mostrando los pagos de mayor a menor

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import modules.getClients as cliente
 import modules.getOficina as oficina
 import modules.getEmpleado as empleado
 import modules.getPedido as pedido
+import modules.getPago as pago
 
 
-print (tabulate(pedido.getAllEnEnero()))
+print (tabulate(pago.getAllAñoPaypal(),tablefmt="grid"))
+
---
+++ modules/getPago.py
@@ -0,0 +1,16 @@
+import storage.pago as pa 
+
+#pagos- filtro 13
+def getAllAñoPaypal():
+    pagosPaypal= []
+    for val in pa.pago:
+        if ("2008") in val.get("fecha_pago") and val.get("forma_pago") == ("PayPal"): 
+            pagosPaypal.append({
+                "codigo_cliente": val.get ("codigo_cliente"),
+                "forma_pago": val.get ("forma_pago"),
+                "fecha_pago": val.get ("fecha_pago"),
+                "total": val.get ("total")
+             })
+    pagosPaypal= sorted(pagosPaypal, key=lambda x: x  ["total"], reverse=True )
+    return pagosPaypal
+

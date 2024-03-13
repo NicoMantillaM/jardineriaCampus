0ac0784aa110ea1eebc749bdaa78e285fdd23280
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-13 09:46:05 -0500

    feat: :construction: creacion de jsons
    pasamos archivos de python a json para poder ingresar a ellosby hadcer funciones con ellos

--- main.py
+++ main.py
@@ -7,44 +7,45 @@ import modules.getEmpleado as empleado
 import modules.getPedido as pedido
 import modules.getPago as pago
 import modules.getProducto as producto
+ 
+producto.getAllData()
+# import sys
+# #def menu():
+# #    contador = 1
+# #     print("Menu Principal")1
+# #     for nombre, objeto in sys.modules.items():
+# #         if nombre.startswith("modules"):
+# #             modulo = getattr(objeto, "__name__", None)
+# #             if(modulo != "modules"):
+# #                 print(f"""{contador}. {modulo.split("get")[-1]} """)
+# #                 contador += 1
+# # menu()
 
-import sys
-#def menu():
-#    contador = 1
-#     print("Menu Principal")1
-#     for nombre, objeto in sys.modules.items():
-#         if nombre.startswith("modules"):
-#             modulo = getattr(objeto, "__name__", None)
-#             if(modulo != "modules"):
-#                 print(f"""{contador}. {modulo.split("get")[-1]} """)
-#                 contador += 1
-# menu()
-
-if(__name__ == "__main__"):
-    while True:
-        print("""
-Menu Principal
-0.Salir
-1.cliente
-2.oficina 
-3.empleado
-4.pedido
-5.pago
-6.producto
-""")
-        opcion = int(input("\nSeleccione una de las opciones: "))
-        if(opcion==1):
-            cliente.menu()
-        elif(opcion==2):
-            oficina.menu()
-        elif(opcion==3):
-            empleado.menu()
-        elif(opcion==4):
-            pedido.menu()
-        elif(opcion==5):
-            pago.menu()
-        elif(opcion==6):
-            producto.menu()
-        elif(opcion==0):
-            break
+#if(__name__ == "__main__"):
+#     while True:
+#         print("""
+# Menu Principal
+# 0.Salir
+# 1.cliente
+# 2.oficina 
+# 3.empleado
+# 4.pedido
+# 5.pago
+# 6.producto
+# """)
+#         opcion = int(input("\nSeleccione una de las opciones: "))
+#         if(opcion==1):
+#             cliente.menu()
+#         elif(opcion==2):
+#             oficina.menu()
+#         elif(opcion==3):
+#             empleado.menu()
+#         elif(opcion==4):
+#             pedido.menu()
+#         elif(opcion==5):
+#             pago.menu()
+#         elif(opcion==6):
+#             producto.menu()
+#         elif(opcion==0):
+#             break
 
--- modules/getProducto.py
+++ modules/getProducto.py
@@ -1,12 +1,20 @@
-import storage.producto as pr
+
 from tabulate import tabulate
+import json
+import requests
+def getAllData():
+    #json-server storage/producto.json -b 5501
+    peticion=requests.get("http://172.16.100.110:5503")
+    data= peticion.json()
+    return data
+    
 
 #Devuelve un listado con todas los productos q pertenecen a la gama ornamentales
 #y q tienen mas de 100 unidades en stock. El listado debera estar ordenado por su precio de venta, mayor a menor
 
 def getAllStocksPriceGama(gama, stock):
     condiciones = []
-    for val in pr.producto:
+    for val in getAllData:
         if(val.get("gama") == gama and val.get("precio_venta") >= stock):
             condiciones.append(val)
 
@@ -42,7 +50,17 @@ REPORTES DE LOS PRODUCTOS
                 gama=input("Ingrese la gama del producto: ")
                 stock=int(input("Ingrese el stock del producto: "))
                 print(tabulate(getAllStocksPriceGama(gama,stock),headers="keys",tablefmt="grid"))
-            elif(opcion==0):
-                break
+            elif(opcion==2):
+                producto= { 
+                "codigo_producto": input("Ingrese el codigo producto"),
+                    "nombre": input("Ingrese el nombre del producto"),
+                    "gama": input("Ingrese el gama del producto"),
+                    "dimensiones": input("Ingrese las dimensiones del producto"),
+                    "proveedor": input("Ingrese el proveedor del producto"),
+                    "descripcion":input ("ingrese descripcion del producto"),
+                    "cantidad_en_stock": int("15"),
+                    "precio_venta": int("14"),
+                    "precio_proveedor":int ("11")
+            }
         except KeyboardInterrupt:
             break

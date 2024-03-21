1db5458508909da73fa59c649dd6fb78c9878d05
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-21 09:21:53 -0500

    feat: :construction: Avance
    añadi un filtro faltante con su opcion, era obtener un producto por su gama en especifico

--- modules/getProducto.py
+++ modules/getProducto.py
@@ -1,6 +1,7 @@
 import os
 from tabulate import tabulate
 import requests
+import modules.getGamas as gG
 
 def getAllData():
     #json-server storage/producto.json -b 5505
@@ -35,17 +36,41 @@ def getAllStocksPriceGama():
     stockPriceGama = []
     for val in getAllData():
         gama = val.get('gama')
-        stock = val.get('cantidad_en_stock')
+        stock = val.get('cantidadEnStock')
         if gama == 'Ornamentales' and stock >= 100:
             stockPriceGama.append({
                 'codigo_producto': val.get('codigo_producto'),
                 'nombre': val.get('nombre'),
                 'gama': val.get('gama'),
-                'cantidad_en_stock': val.get('cantidad_en_stock'),
+                'cantidadEnStock': val.get('cantidadEnStock'),
                 'precio_venta': val.get('precio_venta')
             })
+        else:
+            print("noxd")
     stockPriceGama.sort(key=lambda x: x['precio_venta'], reverse=True)
     return stockPriceGama
+
+
+
+def getAllProductosOrnamentales(gama,stock):
+    condiciones=[]
+    for val in getAllData():
+        if(val.get("gama")==gama) and (val.get("cantidadEnStock")>=stock):
+            condiciones.append(val)
+        def price(val):
+            return val.get("precio_venta")
+        condiciones.sort(key=price, reverse=True)
+        for i, val in enumerate(condiciones):
+            condiciones[i]={
+            "Codigo del Producto":val.get("codigo_producto"),
+            "Nombre":val.get("nombre"),
+            "Gama":val.get("gama"),
+            "Cantidad en stock":val.get("cantidadEnStock"),
+            "Precio de venta":val.get("precio_venta"),
+            "Precio al proveedor":val.get("precio_proveedor")
+        }
+
+    return condiciones
 
 def menu():
     while True:
@@ -53,7 +78,8 @@ def menu():
         print("""
 REPORTES DE LOS PRODUCTOS
 0.Regresar al menu principal
-1.Obtener todos los productos de una categoria ordenando su precio de venta, también que su cantidad de inventario sea superior (ejem: Ornamentales, 100 )
+1.Obtener todos los productos de la categoria Ornamentales, ordenando su precio de venta, también que su cantidad de inventario sea superior (ejem: Ornamentales, 100 )
+2.Obtener todos los productos de una categoria  ordenando su precio de venta, también que su cantidad de inventario sea superior            
 
     -PRESIONA CTRL + C PARA REGRESAR AL MENU PRINCIPAL
 """)
@@ -62,6 +88,12 @@ REPORTES DE LOS PRODUCTOS
             if (opcion==1):
                 print(tabulate(getAllStocksPriceGama(),tablefmt="grid"))
                 input("Precione una tecla para continuar.....")
+            if (opcion==2):   
+                gama= gG.getAllNombre()[(int(input("Seleccione la gama del producto:\n" + "".join([f"\t{i}. {val}\n" for i, val in enumerate(gG.getAllNombre())]))))]
+                stock=int(input("Ingrese el stock del producto: "))
+                print(tabulate(getAllProductosOrnamentales(gama,stock),tablefmt="grid"))
+                input("Presiona cualquier tecla para continuar.....")
+        
             elif(opcion == 0):
                 break
         except KeyboardInterrupt:
